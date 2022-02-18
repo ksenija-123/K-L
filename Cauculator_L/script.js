cbdff1b4f2386b0b26b92ca9863915b676d1be84
LieniteB <liene.arnicane@gmail.com>
2022-02-18 14:56:57 +0200

    Home task - calculator. Liene

---
+++ Cauculator_L/script.js
@@ -0,0 +1,39 @@
+let number_1 = this.prompt("Please write a number.");
+while (isNaN(number_1)) {
+    this.alert("You did not write a number.");
+    number_1 = this.prompt("Please write a number")
+
+}
+// else this.alert("Thank you! It is a number!")
+let operation = this.prompt("Please write a operation.");
+// if (operation!="/" || operation!="*" || operation!="+" || operation!="-") {
+//     this.alert("You did not write a walid operation");
+//     operation = this.prompt("Please write a operation")
+
+
+let number_2 = this.prompt("Please write a number.");
+while (isNaN(number_2)) {
+    this.alert("You did not write a number.");
+    number_2 = this.prompt("Please write a number")
+
+}
+
+if (operation == "/") {
+    result = number_1 / number_2;
+}
+else if (operation == "*") {
+    result = number_1 * number_2;
+}
+else if (operation == "+") {
+    result = parseInt(number_1) + parseInt(number_2);
+}
+else if (operation == "-") {
+    result = parseInt(number_1) - parseInt(number_2);
+}
+
+else
+     this.alert("Error - you did not write a valid operation.")
+console.log(number_1, operation, number_2);
+
+this.alert("The result is " + result);
+
